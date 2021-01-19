6d62714318d0f056c0cf44f9fcb1cee7782acbcc
KelwinHenrique <kelwin-hike@hotmail.com>
2021-01-18 23:44:55 -0300

    added swagger

--- packages/api1/src/app.js
+++ packages/api1/src/app.js
@@ -1,3 +1,6 @@
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger_output.json')
+
 import http from 'http'
 import { config } from './config'
 import express from './core/config/express'
@@ -6,5 +9,6 @@ import api from './api'
 const app = express(config.apiRoot, api)
 const server = http.createServer(app)
 
-
 server.listen(config.port, config.ip, () => console.log('Express server listening on http://%s:%d', config.ip, config.port))
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
--- packages/api2/src/app.js
+++ packages/api2/src/app.js
@@ -1,3 +1,7 @@
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger_output.json')
+
+
 import http from 'http'
 import { config } from './config'
 import express from './core/config/express'
@@ -8,3 +12,5 @@ const server = http.createServer(app)
 
 
 server.listen(config.port, config.ip, () => console.log('Express server listening on http://%s:%d', config.ip, config.port))
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
