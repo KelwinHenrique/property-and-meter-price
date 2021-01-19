0e8a87b7c76e8dc18c19bfa77ff85f54d8e72bec
KelwinHenrique <kelwin-hike@hotmail.com>
2021-01-18 23:28:34 -0300

    Added environment variables

--- packages/api2/src/api/properties/use-cases/get-property-price/services.js
+++ packages/api2/src/api/properties/use-cases/get-property-price/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getMeterPrice = async () => {
-  return await axios.get('http://localhost:3000/api/meters/price')
+  return await axios.get(process.env.URL_API1)
 }
 
 const services = {
--- packages/api2/src/config.js
+++ packages/api2/src/config.js
@@ -1,3 +1,13 @@
+import path from 'path'
+
+if (process.env.NODE_ENV !== 'lab' && process.env.NODE_ENV !== 'prod') {
+  const dotenv = require('dotenv-safe')
+  dotenv.config({
+    allowEmptyValues: true,
+    path: path.join(__dirname, '../.env'),
+    sample: path.join(__dirname, '../.env.example')
+  })
+}
 
 const config = {
   port: 8080,
